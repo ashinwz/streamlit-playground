96f844af7f7677a2ceed26ebfd51cf48e36abc89
John Wang <johnwang@srdlx0009.srd.roche.com>
2022-11-03 14:54:35 +0800

    update dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,6 +152,52 @@ if tabs =='Dashboard':
                         axisRight=None,
                     )
 
+            with mui.Card(key="data", sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}):
+                with mui.Stack(
+                    className="draggable",
+                    alignItems="center",
+                    direction="row",
+                    spacing=1,
+                    sx={
+                        "padding": padding,
+                        "borderBottom": 1,
+                        "borderColor": "divider",
+                    }):
+                    mui.icon.Radar()
+                    mui.Typography("data", sx={"flex": 1})
+                    mui.IconButton(mui.icon.DarkMode)
+
+                    DEFAULT_COLUMNS = [
+                        { "field": 'id', "headerName": 'ID', "width": 90 },
+                        { "field": 'firstName', "headerName": 'First name', "width": 150, "editable": True, },
+                        { "field": 'lastName', "headerName": 'Last name', "width": 150, "editable": True, },
+                        { "field": 'age', "headerName": 'Age', "type": 'number', "width": 110, "editable": True, },
+                    ]
+
+                    DEFAULT_ROWS = [
+                        { "id": 1, "lastName": 'Snow', "firstName": 'Jon', "age": 35 },
+                        { "id": 2, "lastName": 'Lannister', "firstName": 'Cersei', "age": 42 },
+                        { "id": 3, "lastName": 'Lannister', "firstName": 'Jaime', "age": 45 },
+                        { "id": 4, "lastName": 'Stark', "firstName": 'Arya', "age": 16 },
+                        { "id": 5, "lastName": 'Targaryen', "firstName": 'Daenerys', "age": None },
+                        { "id": 6, "lastName": 'Melisandre', "firstName": None, "age": 150 },
+                        { "id": 7, "lastName": 'Clifford', "firstName": 'Ferrara', "age": 44 },
+                        { "id": 8, "lastName": 'Frances', "firstName": 'Rossini', "age": 36 },
+                        { "id": 9, "lastName": 'Roxie', "firstName": 'Harvey', "age": 65 },
+                    ]
+
+                    # with mui.Box(sx={"flex": 1, "minHeight": 0}):
+                with mui.Box(sx={"flex": 1, "minHeight": 0}):
+                    mui.DataGrid(
+                        columns=DEFAULT_COLUMNS,
+                        rows=DEFAULT_ROWS,
+                        pageSize=5,
+                        rowsPerPageOptions=[5],
+                        checkboxSelection=True,
+                        disableSelectionOnClick=True,
+                        onCellEditCommit=True,
+                    )
+
 elif tabs == 'Money':
     st.title("Paper")
     st.write('Name of option is {}'.format(tabs))
