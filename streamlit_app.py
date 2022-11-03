8ba6492d654bcdebe6d3c18b5689adb22b71baa5
John Wang <johnwang@srdlx0009.srd.roche.com>
2022-11-03 14:09:59 +0800

    Push

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,7 @@ st.set_page_config(layout="wide")
 #st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
 with st.sidebar:
-    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
-                             iconName=['dashboard', 'money', 'economy'],
-                             styles = {'navtab': {'background-color':'#111',
-                                                  'color': '#818181',
-                                                  'font-size': '18px',
-                                                  'transition': '.3s',
-                                                  'white-space': 'nowrap',
-                                                  'text-transform': 'uppercase'},
-                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
-                                                                      'cursor': 'pointer'}},
-                                       'iconStyle':{'position':'fixed',
-                                                    'left':'7.5px',
-                                                    'text-align': 'left'},
-                                       'tabStyle' : {'list-style-type': 'none',
-                                                     'margin-bottom': '30px',
-                                                     'padding-left': '30px'}},
-                             key="1")
+    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], iconName=['dashboard', 'money', 'economy'])
 
 # Initialize default data for code editor and chart.
 #
