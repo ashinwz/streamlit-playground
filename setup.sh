73a42b18355f31d4586c32fc3dbd43b3e811fd95
ashinwz <297787884@qq.com>
2022-11-05 13:13:50 +0800

    temp

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-# mkdir -p ~/.streamlit/
+# # mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"john.wang.jw6@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+# echo "\
+# [general]\n\
+# email = \"john.wang.jw6@gmail.com\"\n\
+# " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
